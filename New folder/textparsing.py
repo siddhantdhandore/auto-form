6267d27ac48d229dde57f04afe85232681ffd2bc
siddhantdhandore <siddhantdhandore9@gmail.com>
2021-06-30 17:38:30 +0530

    update

--- New folder/textparsing.py
+++
@@ -1,8 +0,0 @@
-with open ('test123.txt','r') as f:
-    empty_dict={}
-    string=f.readlines()
-    for i in string:
-        if i != '\n':
-            empty_dict[i.split(':')[0].strip()]=(i.split(':')[-1])[:-1].strip()
-    print(empty_dict)
-print(empty_dict['username'])
